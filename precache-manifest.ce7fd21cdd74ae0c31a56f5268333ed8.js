c84fadb7ff3636404a18a338d208c7f483b98250
Kornel Sroka <srokakornel@gmail.com>
2020-06-08 21:04:46 +0200

    Updates

---
+++ precache-manifest.ce7fd21cdd74ae0c31a56f5268333ed8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e109ce846091c63746fcede0456347",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9f2ef609247e04518bce",
+    "url": "/weather-app/static/css/2.67246356.chunk.css"
+  },
+  {
+    "revision": "11d5c719b7a7da1bb862",
+    "url": "/weather-app/static/css/main.54911f0c.chunk.css"
+  },
+  {
+    "revision": "9f2ef609247e04518bce",
+    "url": "/weather-app/static/js/2.bcd7f6ce.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/weather-app/static/js/2.bcd7f6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d5c719b7a7da1bb862",
+    "url": "/weather-app/static/js/main.c6ca4bdf.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  }
+]);
